8943c274a631d6df951a570eb5422f0af98bc41b
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-06-11 11:29:12 +0700

    Добавлен компонент List

---
+++ src/components/list/list.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { IList } from '../../ts/interfaces';
+import './list.scss';
+
+const List = (props: IList) => {
+  return <div className="list"></div>;
+};
+
+export default List;
--- src/components/lists-container/lists-container.tsx
+++ src/components/lists-container/lists-container.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { IListsContainerState } from '../../ts/interfaces';
 import CreateListButtonContainer from '../create-list-button/create-list-button-container';
+import List from '../list/list';
 import './lists-container.scss';
 
 const ListsContainer = (props: IListsContainerState) => {
   const lists = props.lists.map((item) => {
-    return <div className="list" key={item.id}></div>;
+    return <List key={item.id} {...item} />;
   });
 
   return (
