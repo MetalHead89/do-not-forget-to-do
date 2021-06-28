623534c6047f8fcc53d90cee91935de617c993e4
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-06-28 16:15:45 +0700

    Компонент List Изменен в соответствии с интерфейсом IList

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -3,15 +3,22 @@ import { IList } from '../../ts/interfaces';
 import './list.scss';
 
 const List = (props: IList) => {
+  function handleTitleInput() {
+    console.log('dfsdf');
+    // const value = event.target !== null ? event.target.value : ''
+    // props.changeTitle(props.id, event.target.value);
+  }
+
   return (
     <div className="list">
-      {/* <div className="list__title-section">{props.title}</div> */}
       <div className="list__title-section">
         <input
           type="text"
           className="list__title"
           value={props.title}
-          onInput={() => props.changeTitle(props.id, props.title)}
+          onInput={handleTitleInput}
+          // onDoubleClick={() => props.enableTitleEditingMode(props.id)}
+          // readOnly={props.readonly}
         ></input>
       </div>
       <div className="list__items-section">
