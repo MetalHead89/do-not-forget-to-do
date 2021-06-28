99d9190c4acf169ab69927f3e745ddb18cef1edf
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-06-28 16:14:27 +0700

    Измененены интерфейсы IList, IListState и IListsContainer

--- src/ts/interfaces.ts
+++ src/ts/interfaces.ts
@@ -15,18 +15,22 @@ interface IList {
   id: number;
   title: string;
   items: string[];
+  readonly: boolean;
   changeTitle: (id: number, title: string) => void;
+  enableTitleEditingMode: (id: number) => void;
 }
 
 interface IListState {
   id: number;
   title: string;
+  readonly: boolean;
   items: string[];
 }
 
 interface IListsContainer {
   lists: IListState[];
   changeTitle: (id: number, title: string) => void;
+  enableTitleEditingMode: (id: number) => void;
 }
 
 interface IListsContainerState {
