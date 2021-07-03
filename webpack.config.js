b08f2281d0f76d14fe572b666fa707c15378dec1
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-07-03 10:53:23 +0700

    Установлен stylelint

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -21,6 +22,9 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin([{ filename: 'style.css' }]),
+    new StylelintPlugin({
+      configFile: path.resolve(__dirname, './stylelint.config.js'),
+    }),
   ],
   module: {
     rules: [
