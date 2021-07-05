47c02f834303cd5d3c498fee4485adb5eaef084b
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-07-05 08:55:45 +0700

    В интерфейсах IList и IListsContainer функция disableTitleEditingModeCreator переименована в disableTitleEditingMode

--- src/ts/interfaces.ts
+++ src/ts/interfaces.ts
@@ -18,7 +18,7 @@ interface IList {
   readonly: boolean;
   changeTitle: (id: number, title: string) => void;
   enableTitleEditingMode: (id: number) => void;
-  disableTitleEditingModeCreator: (id: number) => void;
+  disableTitleEditingMode: (id: number) => void;
 }
 
 interface IListState {
@@ -32,7 +32,7 @@ interface IListsContainer {
   lists: IListState[];
   changeTitle: (id: number, title: string) => void;
   enableTitleEditingMode: (id: number) => void;
-  disableTitleEditingModeCreator: (id: number) => void;
+  disableTitleEditingMode: (id: number) => void;
 }
 
 interface IListsContainerState {
