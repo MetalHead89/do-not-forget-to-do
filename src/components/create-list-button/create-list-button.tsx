29490dc5442e0754dc9a08330dafedd8dc5ab475
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-08-14 10:06:29 +0700

    Добавлено создание нового списка в mongodb

--- src/components/create-list-button/create-list-button.tsx
+++ src/components/create-list-button/create-list-button.tsx
@@ -1,12 +1,25 @@
+import axios from 'axios';
 import React from 'react';
 import { ICreateListButton } from '../../ts/interfaces';
 import './create-list-button.scss';
+
+function handleCreateListButtonClick() {
+  const promise = axios.post('http://localhost:5000/api/list/add-list', {
+    title: 'Новый список',
+    items: [],
+    isReadonly: false,
+    isFocused: true,
+  });
+  console.log('qwer');
+  return promise.then((response) => response.data);
+}
 
 const CreateListButton = (props: ICreateListButton) => {
   return (
     <button
       className="create-list-button"
-      onClick={() => props.clickToButton()}
+      onClick={ handleCreateListButtonClick }
+      // onClick={() => props.clickToButton()}
     />
   );
 };
