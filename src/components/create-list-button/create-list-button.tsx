77bd8a1f89ba2838fe2cf226661834ae4b70b801
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-09-03 10:19:13 +0700

    Добавлен блок catch в промис добавляющий новый список в БД

--- src/components/create-list-button/create-list-button.tsx
+++ src/components/create-list-button/create-list-button.tsx
@@ -9,15 +9,21 @@ function handleCreateListButtonClick() {
     isReadonly: false,
     isFocused: true,
   });
-  
-  return promise.then((response) => response.data);
+
+  return promise
+    .then((response) => response.data)
+    .catch((err) =>
+      console.log(
+        `Произошла ошибка в процессе добавления нового списка. Текст ошибки: ${err.message}`
+      )
+    );
 }
 
 const CreateListButton = () => {
   return (
     <button
       className="create-list-button"
-      onClick={ handleCreateListButtonClick }
+      onClick={handleCreateListButtonClick}
     />
   );
 };
